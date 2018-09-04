119bb6bfb148fc184d5cbe724b19579d1d9b14d1
Panu Lahtinen <pnuu+git@iki.fi>
2018-09-04 10:43:08 +0300

    Add unit tests for satpy_resampler

--- trollflow_sat/tests/test_satpy_resampler.py
+++ trollflow_sat/tests/test_satpy_resampler.py
@@ -17,16 +17,164 @@
 """Unit tests for SatPy resampler plugin"""
 
 import unittest
-from io import StringIO
-import datetime as dt
-from collections import OrderedDict
+
+try:
+    from unittest.mock import patch, Mock, call
+except ImportError:
+    from mock import patch, Mock, call
 
 from trollflow_sat.satpy_resampler import Resampler
+from trollflow_sat.tests.utils import (write_yaml, PRODUCT_LIST, MockScene,
+                                       METADATA_FILE, PRODUCT_LIST_SATPROJ,
+                                       PRODUCT_LIST_TWO_AREAS)
 
 
 class TestResampler(unittest.TestCase):
 
-    pass
+    def setUp(self):
+        from threading import Lock
+        import six.moves.queue as queue
+
+        self.resampler = Resampler()
+        self.lock = Lock()
+        self.prev_lock = Lock()
+        self.output_queue = queue.Queue()
+        self.context = {'lock': self.lock, 'prev_lock': self.prev_lock,
+                        'output_queue': self.output_queue}
+        self.prodlist = write_yaml(PRODUCT_LIST)
+        self.prodlist_satproj = write_yaml(PRODUCT_LIST_SATPROJ)
+        self.prodlist_two_areas = write_yaml(PRODUCT_LIST_TWO_AREAS)
+
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+        self.assertFalse(self.resampler.use_lock)
+
+    def test_pre_invoke(self):
+        self.assertIsNone(self.resampler.pre_invoke())
+
+    @patch('trollflow_sat.satpy_resampler.utils.acquire_lock')
+    @patch('trollflow_sat.satpy_resampler.utils.release_lock')
+    def test_invoke_none(self, release, acquire):
+        context = self.context
+        context['use_lock'] = True
+        context['content'] = None
+        self.resampler.invoke(context)
+        self.assertIsNone(context['output_queue'].get(timeout=1))
+        self.assertTrue(acquire.called)
+        self.assertTrue(release.called)
+
+    @patch('trollflow_sat.satpy_resampler.utils.covers')
+    @patch('trollflow_sat.satpy_resampler.Pass')
+    def test_invoke_scene_no_coverage(self, Pass, covers):
+        import six.moves.queue as queue
+        scene = MockScene(attrs=METADATA_FILE)
+        context = self.context
+        context['content'] = {'scene': scene,
+                              'extra_metadata': {'eggs': 'spam'}}
+        context['product_list'] = self.prodlist
+        covers.return_value = False
+
+        self.resampler.invoke(context)
+        self.assertTrue(Pass.called)
+        self.assertTrue(any(covers.mock_calls))
+        self.assertRaises(queue.Empty, context['output_queue'].get, timeout=1)
+
+    @patch('trollflow_sat.satpy_resampler.utils.covers')
+    @patch('trollflow_sat.satpy_resampler.Pass')
+    def test_invoke_scene(self, Pass, covers):
+        scene = MockScene(attrs=METADATA_FILE)
+        context = self.context
+        context['content'] = {'scene': scene,
+                              'extra_metadata': {'eggs': 'spam'}}
+        context['product_list'] = self.prodlist
+        context['process_by_area'] = True
+        context['use_lock'] = True
+
+        covers.return_value = True
+
+        self.resampler.invoke(context)
+        self.assertTrue(Pass.called)
+        self.assertTrue(any(covers.mock_calls))
+        self.assertIsNotNone(context['output_queue'].get(timeout=1))
+
+    @patch('trollflow_sat.satpy_resampler.utils.covers')
+    @patch('trollflow_sat.satpy_resampler.Pass')
+    def test_invoke_scene_satproj(self, Pass, covers):
+        scene = MockScene(attrs=METADATA_FILE)
+        context = self.context
+        context['content'] = {'scene': scene,
+                              'extra_metadata': {'eggs': 'spam'}}
+        context['product_list'] = self.prodlist_satproj
+        context['process_by_area'] = True
+        context['use_lock'] = True
+        context['radius'] = None
+        context['cache_dir'] = '/path'
+        covers.return_value = True
+
+        self.resampler.invoke(context)
+        self.assertTrue(Pass.called)
+        self.assertTrue(any(covers.mock_calls))
+        lcl = context['output_queue'].get(timeout=1)
+        self.assertTrue(lcl['scene'] is scene)
+
+    @patch('trollflow_sat.satpy_resampler.utils.covers')
+    @patch('trollflow_sat.satpy_resampler.Pass')
+    def test_invoke_overpass_is_none(self, Pass, covers):
+        scene = MockScene(attrs=METADATA_FILE)
+        context = self.context
+        context['content'] = {'scene': scene,
+                              'extra_metadata': {'eggs': 'spam'}}
+        context['product_list'] = self.prodlist
+        context['process_by_area'] = True
+        Pass.return_value = None
+
+        self.resampler.invoke(context)
+        self.assertFalse(any(covers.mock_calls))
+        self.assertIsNotNone(context['output_queue'].get(timeout=1))
+
+    @patch('trollflow_sat.satpy_resampler.utils.covers')
+    @patch('trollflow_sat.satpy_resampler.Pass')
+    def test_invoke_two_areas_by_area(self, Pass, covers):
+        scene = MockScene(attrs=METADATA_FILE)
+        context = self.context
+        context['content'] = {'scene': scene,
+                              'extra_metadata': {'eggs': 'spam'}}
+        context['product_list'] = self.prodlist_two_areas
+        context['process_by_area'] = True
+        self.resampler.invoke(context)
+        # Every second item should be None
+        res = context['output_queue'].get(timeout=1)
+        self.assertTrue(res['scene'].attrs['area_id'] == 'area1')
+        res = context['output_queue'].get(timeout=1)
+        self.assertIsNone(res)
+        res = context['output_queue'].get(timeout=1)
+        self.assertTrue(res['scene'].attrs['area_id'] == 'area2')
+        res = context['output_queue'].get(timeout=1)
+        self.assertIsNone(res)
+        self.assertEqual(context['output_queue'].qsize(), 0)
+
+    @patch('trollflow_sat.satpy_resampler.utils.covers')
+    @patch('trollflow_sat.satpy_resampler.Pass')
+    def test_invoke_two_areas_together(self, Pass, covers):
+        scene = MockScene(attrs=METADATA_FILE)
+        context = self.context
+        context['content'] = {'scene': scene,
+                              'extra_metadata': {'eggs': 'spam'}}
+        context['product_list'] = self.prodlist_two_areas
+        context['process_by_area'] = False
+
+        self.resampler.invoke(context)
+        # Only Scene objects in the queue
+        res = context['output_queue'].get(timeout=1)
+        self.assertTrue(res['scene'].attrs['area_id'] == 'area1')
+        res = context['output_queue'].get(timeout=1)
+        self.assertTrue(res['scene'].attrs['area_id'] == 'area2')
+        self.assertEqual(context['output_queue'].qsize(), 0)
+
+    def test_post_invoke(self):
+        self.assertIsNone(self.resampler.post_invoke())
 
 
 def suite():
